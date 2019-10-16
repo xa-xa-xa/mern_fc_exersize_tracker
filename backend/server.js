b891b1c9694b72c0f45a35342d7bc1ec5a89b098
xa-xa-xa <askrasn@gmail.com>
2019-10-16 12:40:14 -0700

    trying setup db to heroku

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,38 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-require('dotenv').config()
+require('dotenv').config();
 
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-mongoose.connect(process.env.ATLAS_URI, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useCreateIndex: true
-    })
-    .catch(e => console.error("ERROR: ", e.message));
+// mongoose.connect(process.env.ATLAS_URI, {
+//         useUnifiedTopology: true,
+//         useNewUrlParser: true,
+//         useCreateIndex: true
+//     })
+//     .catch(e => console.error("ERROR: ", e.message));
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .catch(e => console.error('ERROR: ', e.message));
 
-const connection = mongoose.connection
-connection.once('open', () => console.log("\n*** MongoDB database connection established successfully."));
+const connection = mongoose.connection;
+connection.once('open', () =>
+  console.log('\n*** MongoDB database connection established successfully.')
+);
 
-const exercisesRouter = require('./routes/exercises')
-const usersRouter = require('./routes/users')
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 
-app.use('/exercises', exercisesRouter)
-app.use('/users', usersRouter)
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
-
-
-
-
-
-app.listen(port, () => console.log(`Server is running on port: ${port}`))
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
